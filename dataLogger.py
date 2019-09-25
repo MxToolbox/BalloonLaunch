d2470d2d11c3f7d6fc68ecfe4dcbaf51e478f949
rachale <rachale@users.noreply.github.com>
2019-09-24 21:10:43 -0500

    Added pressure altitude calculation to data logging.

--- dataLogger.py
+++ dataLogger.py
@@ -3,25 +3,31 @@ import csvLog
 from datetime import datetime, timedelta 
 from sense_hat import SenseHat
 
+def pressureAltitude(millibars):
+    # https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf
+    # takes millibars, returns pressure altitude in feet.
+    return round(((1 - (millibars / 1013.25)** 0.190284)) * 145366.45, 0)
+
 sense = SenseHat()
-values = [0]*9
-headers = [0]*9
-headers = ["time","temp","humidity","pressure","pitch","roll","yaw","lat","lon"]
-print('| {0:>26} | {1:>6} | {2:>8} | {3:>8} | {4:>6} | {5:>6} | {6:>6} | {7:>6} | {8:>6} |'.format(*headers))
+values = [0]*10
+headers = [0]*10
+headers = ["time","temp","humidity","pressure","pressure alt","pitch","roll","yaw","lat","lon"]
+print('| {0:>26} | {1:>6} | {2:>8} | {3:>8} | {4:>12} | {5:>6} | {6:>6} | {7:>6} | {8:>6} | {8:>6} |'.format(*headers))
 csvLog.writeCsvLog(headers)
 while True:
     values[0] = str(datetime.now())
     values[1] = round(sense.temp, 3)    # celsius
     values[2] = round(sense.humidity, 3) # %
     values[3] = round(sense.pressure, 3) # millibars
+    values[4] = round(pressureAltitude(sense.pressure), 0) # feet
     orientation = sense.get_orientation_degrees()
-    values[4] = round(orientation["pitch"], 0)
-    values[5] = round(orientation["roll"], 0)
-    values[6] = round(orientation["yaw"], 0)
-    values[7] = "0.0"  # LAT (from another sensor)
-    values[8] = "0.0"  # LON (from another sensor)
+    values[5] = round(orientation["pitch"], 0)
+    values[6] = round(orientation["roll"], 0)
+    values[7] = round(orientation["yaw"], 0)
+    values[8] = "0.0"  # LAT (from another sensor)
+    values[9] = "0.0"  # LON (from another sensor)
     csvLog.writeCsvLog(values)
 
-    print('| {0:>26} | {1:>6} | {2:>8} | {3:>8} | {4:>6} | {5:>6} | {6:>6} | {7:>6} | {8:>6} |'.format(*values))
+    print('| {0:>26} | {1:>6} | {2:>8} | {3:>8} | {4:>12} | {5:>6} | {6:>6} | {7:>6} | {8:>6} | {8:>6} |'.format(*values))
     time.sleep(5)
     #sense.show_message("Hello world!")
