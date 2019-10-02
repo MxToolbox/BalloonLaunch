33820245b389a34e93094e664f4a04b7a9cf787e
rachale <rachale@users.noreply.github.com>
2019-10-02 11:51:17 -0500

    Wrapped dataLogger loop in try...except;  light will flash red instead of white.

--- dataLogger.py
+++ dataLogger.py
@@ -23,45 +23,49 @@ csvLog.writeCsvLog(headers)
 LogFreqSeconds = 1
 lastPressureAlt = pressureAltitude(sense.pressure)
 while True:
-    sense.clear(255,255,255)  # Strobe effect while reading sensors
-    currentTime = datetime.now()
-    values[0] = str(currentTime)
-    values[1] = round(sense.temp, 3)    # celsius
-    values[2] = round(sense.humidity, 3) # %
-    values[3] = round(sense.pressure, 3) # millibars
+    try:
+        sense.clear(255,255,255)  # Strobe effect while reading sensors
+        currentTime = datetime.now()
+        values[0] = str(currentTime)
+        values[1] = round(sense.temp, 3)    # celsius
+        values[2] = round(sense.humidity, 3) # %
+        values[3] = round(sense.pressure, 3) # millibars
 
-    pressureAlt = pressureAltitude(sense.pressure)
-    values[4] = round(pressureAlt, 0) # feet
+        pressureAlt = pressureAltitude(sense.pressure)
+        values[4] = round(pressureAlt, 0) # feet
 
-    values[5] = round((pressureAlt - lastPressureAlt) / LogFreqSeconds, 1) # feet per second
-    orientation = sense.get_orientation_degrees()
+        values[5] = round((pressureAlt - lastPressureAlt) / LogFreqSeconds, 1) # feet per second
+        orientation = sense.get_orientation_degrees()
 
-    #sense.set_imu_config(False, True, True)  # gyroscope / accelerometer only
-    values[6] = round(orientation["pitch"], 0)
-    values[7] = round(orientation["roll"], 0)
-    values[8] = round(orientation["yaw"], 0)
+        #sense.set_imu_config(False, True, True)  # gyroscope / accelerometer only
+        values[6] = round(orientation["pitch"], 0)
+        values[7] = round(orientation["roll"], 0)
+        values[8] = round(orientation["yaw"], 0)
 
-   # takessense.set_imu_config(True, False, False)  # compass only
-    #values[9] = round(sense.get_compass(), 2)
-    values[10] = tracker.gpsd.fix.latitude  # LAT (from another sensor)
-    values[11] = tracker.gpsd.fix.longitude  # LON (from another sensor)
-    values[12] = tracker.gpsd.fix.altitude  
-    values[13] = tracker.gpsd.fix.speed  
-    values[14] = tracker.gpsd.fix.climb  
-    values[15] = tracker.gpsd.fix.track 
-    values[16] = tracker.gpsd.utc 
+    # takessense.set_imu_config(True, False, False)  # compass only
+        #values[9] = round(sense.get_compass(), 2)
+        values[10] = tracker.gpsd.fix.latitude  # LAT (from another sensor)
+        values[11] = tracker.gpsd.fix.longitude  # LON (from another sensor)
+        values[12] = tracker.gpsd.fix.altitude  
+        values[13] = tracker.gpsd.fix.speed  
+        values[14] = tracker.gpsd.fix.climb  
+        values[15] = tracker.gpsd.fix.track 
+        values[16] = tracker.gpsd.utc 
 
-    csvLog.writeCsvLog(values)
-    #print(formatStr.format(*values))
-    i = 0
-    for v in values:
-        formatStr = '| {0:>15} | {1:>26} |'
-        print(formatStr.format(headers[i],  v))
-        i = i + 1
-    print('________________________________________________')
+        csvLog.writeCsvLog(values)
+        #print(formatStr.format(*values))
+        i = 0
+        for v in values:
+            formatStr = '| {0:>15} | {1:>26} |'
+            print(formatStr.format(headers[i],  v))
+            i = i + 1
+        print('________________________________________________')
 
-    lastPressureAlt = pressureAlt
-    telemetry.values = values
-    sense.clear()  # Strobe off
+        lastPressureAlt = pressureAlt
+        telemetry.values = values
+        sense.clear()  # Strobe off
+    except:
+        print("Unhandled exception!")
+        sense.clear(255,0,0)  # Strobe effect while reading sensors
     time.sleep(LogFreqSeconds)
     #sense.show_message("Hello world!")
